404b1c6b94e0fc4ff69e9cb84128f885219585a6
rvlutya <77841957+rvlutya@users.noreply.github.com>
2021-04-11 09:20:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,45 +1,110 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+var dog,sadDog,happyDog,garden,washroom, database;
+var foodS,foodStock;
+var fedTime,lastFed,currentTime;
+var feed,addFood;
+var foodObj;
+var gameState,readState;
 
-var engine, world;
-var box1, box2, box3,box4;
-var hero,monster,rope,ground;
-
-function preload() {
-  bg = loadImage("gamingbackgroung.png");
+function preload(){
+sadDog=loadImage("Images/Dog.png");
+happyDog=loadImage("Images/happy dog.png");
+garden=loadImage("Images/Garden.png");
+washroom=loadImage("Images/Wash Room.png");
+bedroom=loadImage("Images/Bed Room.png");
 }
 
 function setup() {
-  createCanvas(3000, 700);
-  engine = Engine.create();
-  world = engine.world;
+  database=firebase.database();
+  createCanvas(400,500);
+  
+  foodObj = new Food();
 
-  ground = new Ground(600, 600, 1200, 20);
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
 
-  hero = new Hero(400,800,250);
-  rope = new Rope(hero.body, { x: 500, y: 50 });
-  monster = new Monster(1100,550,300);
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastFed=data.val();
+  });
 
-  box1 = new Box(600, 100, 70, 70);
-  box2 = new Box(900, 100, 70, 70);
-  box3 = new Box(900, 100, 70, 70);
-  box4 = new Box(900, 100, 70, 70);
+  //read game state from database
+  readState=database.ref('gameState');
+  readState.on("value",function(data){
+    gameState=data.val();
+  });
+   
+  dog=createSprite(200,400,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+  
+  feed=createButton("Feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
 
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
 }
 
 function draw() {
-  background(bg);
-  Engine.update(engine);
-  ground.display();
-  box1.display();
-  box2.display();
-  box3.display();
-  box4.display()
+  currentTime=hour();
+  if(currentTime==(lastFed+1)){
+      update("Playing");
+      foodObj.garden();
+   }else if(currentTime==(lastFed+2)){
+    update("Sleeping");
+      foodObj.bedroom();
+   }else if(currentTime>(lastFed+2) && currentTime<=(lastFed+4)){
+    update("Bathing");
+      foodObj.washroom();
+   }else{
+    update("Hungry")
+    foodObj.display();
+   }
+   
+   if(gameState!="Hungry"){
+     feed.hide();
+     addFood.hide();
+     dog.remove();
+   }else{
+    feed.show();
+    addFood.show();
+    dog.addImage(sadDog);
+   }
+ 
+  drawSprites();
+}
 
-  hero.display();
-  rope.display();
-  monster.display();
+//function to read food Stock
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
 
+
+//function to update food stock and last fed time
+function feedDog(){
+  dog.addImage(happyDog);
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    Food:foodObj.getFoodStock(),
+    FeedTime:hour(),
+    gameState:"Hungry"
+  })
+}
+
+//function to add food in stock
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+
+//update gameState
+function update(state){
+  database.ref('/').update({
+    gameState:state
+  })
 }
